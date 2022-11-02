5c281b9005e6208e24736d950e1ea08795e1cb57
Marcos Figueiredo <marcos.figueiredo.masf@gmail.com>
2022-11-02 01:47:35 -0300

    adicionado função de mudança de cor do robotron

--- js/main.js
+++ js/main.js
@@ -1,5 +1,34 @@
+const robo = document.querySelector("[data-robo]")
+const robos = [
+    {
+        cor: "img/Robotron 2000 - Amarelo/Robotron 2000 - Amarelo.png"
+    },
+
+    {
+        cor: "img/Robotron 2000 - Azul/Robotron 2000 - Azul.png"
+    },
+
+    {
+        cor: "img/Robotron 2000 - Branco/Robotron 2000 - Branco.png"
+    },
+
+    {
+        cor: "img/Robotron 2000 - Preto/Robotron 2000 - Preto.png"
+    },
+
+    {
+        cor: "img/Robotron 2000 - Rosa/Robotron 2000 - Rosa.png"
+    },
+
+    {
+        cor: "img/Robotron 2000 - Vermelho/Robotron 2000 - Vermelho.png"
+    }
+]
+
 const controle = document.querySelectorAll("[data-controle]")
+
 const estatisticas = document.querySelectorAll("[data-estatistica]")
+
 const pecas = {
     "bracos": {
         "forca": 29,
@@ -57,3 +86,17 @@ function autualizaEstatisticas(peca) {
     })
 }
 
+
+// Mudar cor do Robotron
+let indice = 0
+
+robo.addEventListener("click", () => {
+
+    if (indice <= 4) {
+        indice++
+    } else {
+        indice = 0
+    }
+
+    robo.setAttribute("src", robos[indice].cor)
+})
